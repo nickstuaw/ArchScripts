cafe4d42ceb28bd5485199d3dbacc44048b1b044
nsgw <dev@nsgw.xyz>
2021-11-01 23:48:04 +0000

    Update vm_uefi.sh

--- vm_uefi.sh
+++ vm_uefi.sh
@@ -23,21 +23,7 @@ echo "Mounted the filesystem."
 pacstrap /mnt base linux-zen linux-firmware vim
 echo "Installed zen kernel and linux firmware."
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-echo "Chrooted into the new system."
-mkdir efi
-echo "Created efi mount point."
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock --systohc
-locale-gen
-localectl set-locale LANG=en_GB.UTF-8
-echo "Time and language set."
-echo -n "Choose a device name (hostname): " && read && echo $REPLY > /etc/hostname
-echo "Set the root password." && passwd
-mount /dev/sda1 /efi
-pacman -S grub efibootmgr amd-ucode --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt bash <(https://raw.githubusercontent.com/nsgwick/ArchScripts/main/vm_uefi_chroot_script.sh)
 exit
 umount -R /mnt
 reboot
