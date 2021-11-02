3eb65540d8c092b07b3dff71ae73ef883b9df9ac
nsgw <dev@nsgw.xyz>
2021-11-02 10:21:30 +0000

    Update vm_uefi.sh

--- vm_uefi.sh
+++ vm_uefi.sh
@@ -1,30 +1,5 @@
 #!/bin/sh
 # Written by Nick (https://github.com/nsgwick (https://nsgw.xyz))
-runInChroot() {
-  # Written by Nick (https://github.com/nsgwick (https://nsgw.xyz))
-  echo "Chrooted into the new system."
-  echo "Created efi mount point."
-  # Set the local timezone
-  ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-  hwclock --systohc
-  # Generate the locale
-  locale-gen
-  localectl set-locale LANG=en_GB.UTF-8
-  echo "Time and language set."
-  # Set system details
-  echo -n "Choose a device name (hostname): " && read && echo $REPLY > /etc/hostname
-  echo "Set the root password." && passwd
-  # Install GRUB bootloader
-  mkdir efi
-  mount /dev/sda1 /efi
-  pacman -S grub efibootmgr amd-ucode --noconfirm
-  grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-  grub-mkconfig -o /boot/grub/grub.cfg
-  # Enable dynamic IP usage
-  dhcpcd -k
-  dhcpcd
-  systemctl enable dhcpcd
-}
 # Use timedatectl to ensure the system clock is accurate: 
 timedatectl set-ntp true
 echo "Using time datectl."
@@ -46,7 +21,7 @@ echo "Mounted the filesystem."
 pacstrap /mnt base linux-zen linux-firmware dhcpcd vim
 echo "Installed zen kernel and linux firmware."
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt runInChroot()
+arch-chroot /mnt bash <(curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/vm_uefi_chroot_script.sh)
 umount -R /mnt
 reboot
 
