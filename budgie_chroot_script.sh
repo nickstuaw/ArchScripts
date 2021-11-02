ff1dbf7ef9e3d7b0aa8d4a832da164b386f8ff0d
nsgw <dev@nsgw.xyz>
2021-11-02 11:05:49 +0000

    Create budgie_chroot_script.sh

---
+++ budgie_chroot_script.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Written by Nick (https://github.com/nsgwick (https://nsgw.xyz))
+echo "Chrooted into the new system."
+echo "Created efi mount point."
+# Set the local timezone
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc
+# Generate the locale
+locale-gen
+localectl set-locale LANG=en_GB.UTF-8
+echo "Time and language set."
+# Set system details
+echo -n "Choose a device name (hostname): " && read && echo $REPLY > /etc/hostname
+echo "Set the root password." && passwd
+# Install GRUB bootloader
+mkdir efi
+mount /dev/sda1 /efi
+pacman -S grub efibootmgr amd-ucode --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+# Download and set the wallpaper
+mkdir /usr/share/backgrounds/nsgw
+curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/archlinux.png > /usr/share/backgrounds/nsgw/default.png
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/nsgw/default.png'
+# Enable dynamic IP usage
+dhcpcd -k
+dhcpcd
+systemctl enable dhcpcd
