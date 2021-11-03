4fc047f66fa865a117baf1d3569d0db89f7c247f
nsgw <dev@nsgw.xyz>
2021-11-03 23:51:13 +0000

    Update budgie_chroot_script.sh

--- budgie_chroot_script.sh
+++ budgie_chroot_script.sh
@@ -21,9 +21,9 @@ pacman -S grub efibootmgr amd-ucode --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 # Download wallpaper
-curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/archlinux.png > /usr/share/backgrounds/budgie/default.png
+curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/archlinux.jpg > /usr/share/backgrounds/budgie/default.jpg
 # Set the wallpaper
-su -c "gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/budgie/default.png" $uname
+su -c "gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/budgie/default.jpg" $uname
 # Enable dynamic IP usage
 dhcpcd -k
 dhcpcd
