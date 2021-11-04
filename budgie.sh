bffc5bad24f3a61f04595f726ab56e94c4d0c8b5
nsgw <dev@nsgw.xyz>
2021-11-04 15:55:45 +0000

    Remove irrelevant echos.

--- budgie.sh
+++ budgie.sh
@@ -3,25 +3,19 @@
 # This script includes GDM, budgie-desktop, and pipewire.
 # Use timedatectl to ensure the system clock is accurate: 
 timedatectl set-ntp true
-echo "Using time datectl."
 # Download database files
 pacman -Sy
-echo "Downloaded database files."
 # Install gparted
 pacman -S parted --noconfirm
-echo "Intalled gparted."
 # Partition for UEFI
 parted /dev/sda mklabel gpt
-echo "Created a GPT table."
 parted /dev/sda mkpart EFI fat32 1MiB 512MiB set 1 esp on mkpart swap linux-swap 512MiB 1024MiB mkpart rootandfs ext4 1536MiB 100%
 mkfs.vfat -F 32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt
-echo "Mounted the filesystem."
 # PACKAGES
 pacstrap /mnt base linux-zen linux-firmware dhcpcd sudo vim ufw nvidia pipewire pipewire-alsa pipewire-jack pipewire-pulse xorg-server xorg-apps gdm budgie-desktop budgie-screensaver gnome-shell gnome-tweaks gnome-control-center kitty
-echo "Installed zen kernel and linux firmware."
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt bash <(curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/budgie_chroot_script.sh)
 umount -R /mnt
