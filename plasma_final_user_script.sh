e7595518f3ea89ea0b0faa83685050093901e509
nsgw <dev@nsgw.xyz>
2021-11-05 20:47:00 +0000

    Create plasma_final_user_script.sh

---
+++ plasma_final_user_script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# =====================================================
+# =============        RUN AS USER        =============
+# =====================================================
+
+cd ~
+
+# Install yay
+echo "INSTALLING YAY..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
