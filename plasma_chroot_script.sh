96b680b20bc98e814fde52a9b9f2f336900498e5
nsgw <dev@nsgw.xyz>
2021-11-05 11:59:57 +0000

    Create plasma_chroot_script.sh

---
+++ plasma_chroot_script.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Written by Nick (https://github.com/nsgwick (https://nsgw.xyz))
+echo "Chrooted into the new system."
+# Set the local timezone
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc
+# Generate the locale
+locale-gen
+localectl set-locale LANG=en_GB.UTF-8
+# Set system details
+echo -n "Choose a device name (hostname): " && read && echo $REPLY > /etc/hostname
+echo "Set the root password." && passwd
+echo -n "Choose a username: " && read uname && useradd -m -G wheel $uname
+echo "Set the password for $uname." && passwd $uname
+# Install GRUB bootloader
+mkdir efi
+mount /dev/sda1 /efi
+pacman -S grub efibootmgr amd-ucode --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+#to-do: Download wallpaper
+#curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/archlinux.jpg > /usr/share/backgrounds/budgie/default.jpg
+# Enable dynamic IP usage
+dhcpcd -k
+dhcpcd
+# Enable what needs to be enabled
+systemctl enable dhcpcd lightdm
