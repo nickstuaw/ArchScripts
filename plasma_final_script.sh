ce61d901691242248ee45c5409556f7926ba2156
nsgw <dev@nsgw.xyz>
2021-11-06 15:05:52 +0000

    Update plasma_final_script.sh

--- plasma_final_script.sh
+++ plasma_final_script.sh
@@ -5,9 +5,14 @@
 
 cd ~
 
+# Pacman packages
+sudo pacman -S ark kbackup
+sudo pacman -S flameshot
 # Install yay
 echo "INSTALLING YAY..."
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay
+yay -Syu
+yay -S microsoft-edge-stable-bin --noprovides --noremovemake --answeredit 1 --answerclean N --answerdiff N --save
+yay -S latte-dock
