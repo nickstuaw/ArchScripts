c479b57ef5db294228b80899ca5cc40668c6e231
nsgw <dev@nsgw.xyz>
2021-11-09 10:52:30 +0000

    Update plasma_final_script.sh

--- plasma_final_script.sh
+++ plasma_final_script.sh
@@ -6,7 +6,7 @@
 cd ~
 
 # Pacman packages
-sudo pacman -S ark kbackup flameshot discord obs-studio code --noconfirm
+sudo pacman -S ark kbackup flameshot discord obs-studio code dotnet-runtime dotnet-sdk mono --noconfirm
 # Install yay
 echo "INSTALLING YAY..."
 git clone https://aur.archlinux.org/yay.git
@@ -14,4 +14,8 @@ cd yay
 echo y | makepkg -si
 yay -Syu
 echo y | yay -S microsoft-edge-stable-bin --noprovides --noremovemake --answeredit 1 --answerclean N --answerdiff N --save
-echo y | yay -S latte-dock discord intellij-idea-ultimate-edition rider bootstrap-studio
+echo y | yay -S latte-dock intellij-idea-ultimate-edition rider bootstrap-studio
+# Install packettracer
+git clone https://aur.archlinux.org/packettracer.git
+cd packettracer
+echo "Log into Cisco Networking Academy and go to\nResources menu > Download packet tracer.\nwnload the .deb file and put it in this directory then build and install the package.\nSee https://wiki.archlinux.org/title/PacketTracer for more details." > touch instructions.txt
