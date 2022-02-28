4f5ab576fa5ee77d91ec3d22035d9a6c5a844d32
nsgw <dev@nsgw.xyz>
2022-02-28 22:11:24 +0000

    Update plasma_final_script.sh

--- plasma_final_script.sh
+++ plasma_final_script.sh
@@ -8,10 +8,7 @@ cd ~
 # Pacman packages
 sudo pacman -S ark kbackup flameshot discord obs-studio code --noconfirm
 # Install yay
-echo "INSTALLING YAY..."
-git clone https://aur.archlinux.org/yay.git
-cd yay
-echo y | makepkg -si
-yay -Syu
+bash <(curl https://raw.githubusercontent.com/nsgwick/ArchScripts/main/yay_user_install.sh)
+# Install packages
 echo y | yay -S microsoft-edge-stable-bin --noprovides --noremovemake --answeredit 1 --answerclean N --answerdiff N --save
 echo y | yay -S latte-dock discord intellij-idea-ultimate-edition rider bootstrap-studio
