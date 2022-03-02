75c6d9e27cc618ba753fdd074006fa8d9059e74a
nsgw <dev@nsgw.xyz>
2022-03-02 18:22:06 +0000

    Update budgie.sh

--- budgie.sh
+++ budgie.sh
@@ -8,12 +8,12 @@ pacman -Sy
 # Install gparted
 pacman -S parted --noconfirm
 # Partition for UEFI
-parted /dev/sda mklabel gpt
-parted /dev/sda mkpart EFI fat32 1MiB 512MiB set 1 esp on mkpart swap linux-swap 512MiB 1024MiB mkpart rootandfs ext4 1536MiB 100%
+parted /dev/nvme1n1 mklabel gpt
+parted /dev/nvme1n1 mkpart EFI fat32 1MiB 512MiB set 1 esp on mkpart swap linux-swap 512MiB 1024MiB mkpart rootandfs ext4 1536MiB 100%
 mkfs.vfat -F 32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt
+mkswap /dev/nvme1n1p2
+mkfs.ext4 /dev/nvme1n1p3
+mount /dev/nvme1n1p3 /mnt
 # PACKAGES
 pacstrap /mnt base linux-zen linux-firmware dhcpcd sudo vim ufw nvidia pipewire pipewire-alsa pipewire-jack pipewire-pulse xorg-server xorg-apps gdm budgie-desktop budgie-screensaver gnome-shell gnome-tweaks gnome-control-center kitty
 genfstab -U /mnt >> /mnt/etc/fstab
